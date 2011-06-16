ab02a481275bd7f9df5db67c1c1c6a6754e1d7e0
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2011-06-16 18:16:29 +0100

    Added flutie and capustrano

---
+++ config/deploy.rb
@@ -0,0 +1,65 @@
+# RVM bootstrap
+$:.unshift(File.expand_path("~/.rvm/lib"))
+require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.2-p180'
+set :rvm_type, :system
+
+# bundler bootstrap
+require 'bundler/capistrano'
+
+# main details
+set :application, "proman.swan.ac.uk"
+role :web, "proman.swan.ac.uk"
+role :app, "proman.swan.ac.uk"
+role :db,  "proman.swan.ac.uk", :primary => true
+
+# server details
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :deploy_to, "/var/www/proman.swan.ac.uk"
+set :deploy_via, :remote_cache
+set :user, "passenger"
+set :use_sudo, false
+
+
+# Repo details
+set :scm, :git
+set :scm_username, "passenger"
+set :repository, "git@github.com:cpjobling/proman_mmxii.git"
+set :branch, "master"
+set :git_enable_submodules, 1
+
+# tasks
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Symlink shared resources on each release - not used"
+  task :symlink_shared, :roles => :app do
+    #run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
